3985470e6090487f84e407eb9d07726df0f67f4e
zhaochao <zhaochao@rd.netease.com>
2019-04-24 20:56:18 +0800

    add

--- IOS界面/IOS界面/UiView.m
+++ IOS界面/IOS界面/UiView.m
@@ -39,6 +39,14 @@
     NSLog(@"view1的值 = x:%f;y:%f;width:%f;height:%f", view1.frame.origin.x, view1.frame.origin.y, view1.frame.size.width, view1.frame.size.height);
     NSLog(@"view1 center的值 = x:%f;y:%f;", view1.center.x, view1.center.y);
     
+//    UILabel* label1 = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, view1.frame.size.width, view1.frame.size.height)];
+//    label1.text = @"一";
+//    label1.textAlignment = NSTextAlignmentJustified;
+//    label1.textColor = [UIColor whiteColor];
+//    [view1 addSubview:label1];
+    
+    
+    
     
     //view2
     UIView* view2 = [[UIView alloc] init];
@@ -49,12 +57,20 @@
     NSLog(@"view2 center的值 = x:%f;y:%f;", view2.center.x, view2.center.y);
     
     
+    
     //view3
     UIView* view3 = [[UIView alloc] init];
-    view3.frame = CGRectMake(50, 50, 50, 50);
+    view3.frame = CGRectMake(50, 50, 100, 50);
     view3.backgroundColor = [UIColor redColor];
     view3.tag = 3;
     [view2 addSubview:view3];
+    
+    
+    //view4
+    UIView* view4 = [[UIView alloc] initWithFrame:CGRectMake(60, 60, 50, 50)];
+    view4.backgroundColor = [UIColor redColor];
+    [view2 addSubview:view4];
+    
     
     //子视图，是个数组
     NSArray* subViewsArray = [view1 subviews];
@@ -74,8 +90,57 @@
     
     
     //设置层级关系
-
+    // 1. 同一个父视图，现在加入的会被后加入的view会给覆盖
+    // 2. 子视图跟随父视图进行层级 遮挡
     
+    
+    
+    //交互层级
+    //[view2 exchangeSubviewAtIndex:0 withSubviewAtIndex:1];
+    
+    //1. 插入view到指定层
+     UIView* view5 = [[UIView alloc] initWithFrame:CGRectMake(70, 70, 50, 50)];
+     view5.backgroundColor = [UIColor blackColor];
+    
+    [view2 insertSubview:view5 atIndex:0];
+    
+    //[view2 insertSubview:view5 aboveSubview:view4];
+    
+    //[view2 insertSubview:view5 belowSubview:view3];
+    
+    //2. 最顶层
+    //[view2 bringSubviewToFront:view5];
+    
+    //3. 最底层
+    [view2 sendSubviewToBack:view5];
+    
+    
+    
+    //自适应
+    UIView* backView = [[UIView alloc] initWithFrame:CGRectMake([UIScreen mainScreen].bounds.size.width / 2 - 10, 400, 50, 50)];
+    backView.tag = 1001;
+    backView.backgroundColor = [UIColor redColor];
+    backView.autoresizesSubviews = YES; //开始自适应
+    [self.view addSubview:backView];
+    
+    UIView* innerView = [[UIView alloc] initWithFrame:CGRectMake(5, 5, 40, 40)];
+    innerView.backgroundColor = [UIColor blackColor];
+    innerView.autoresizingMask = UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleBottomMargin| UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    [backView addSubview:innerView];
+    
+    
+    UIButton* btn = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    btn.frame = CGRectMake(100, 500, 100, 20);
+    btn.backgroundColor = [UIColor redColor];
+    [btn setTitle:@"点击我" forState:UIControlStateNormal];
+    [btn addTarget:self action:@selector(btnClick) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn];
+    
+}
+
+-(void)btnClick{
+    UIView* view = [self.view viewWithTag:1001];
+    view.frame = CGRectMake(view.frame.origin.x - 5, view.frame.origin.y - 5, view.frame.size.width + 10, view.frame.size.height+10);
 }
 
 -(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
